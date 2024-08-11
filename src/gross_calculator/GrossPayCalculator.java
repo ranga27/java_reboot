24fdfd6aacbaf38716b675568d9c09da249fadda
ranga27 <sarang.radke@gmail.com>
2024-08-11 21:34:07 +0100

    cleanup and added commentary

--- src/gross_calculator/GrossPayCalculator.java
+++ src/gross_calculator/GrossPayCalculator.java
@@ -4,15 +4,24 @@ import java.util.Scanner;
 
 public class GrossPayCalculator {
     public static void main(String[] args) {
+        // Get the number of hours worked
         int hours = 0;
-        System.out.println("How many hours did yuo work?");
+        System.out.println("How many hours did you work?");
+
         Scanner scanner = new Scanner(System.in);
         hours = scanner.nextInt();
+
+        // Get the hourly pay rate
         double payRate = 0;
-        System.out.println("What is your payrate?");
+        System.out.println("What is your pay rate?");
         payRate = scanner.nextDouble();
+
         scanner.close();
+
+        //Multiply hours and pay rate
         double grossPay = hours * payRate;
+
+        //Display result
         System.out.println("Gross pay: " + grossPay);
     }
 }
