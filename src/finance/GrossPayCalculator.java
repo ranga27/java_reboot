2ce7812a019539032cf7c4f0594fdc5d92b9ba95
ranga27 <sarang.radke@gmail.com>
2024-08-11 21:46:40 +0100

    cleanup and added QuotaChecker.java

--- src/finance/GrossPayCalculator.java
+++ src/finance/GrossPayCalculator.java
@@ -1,20 +1,17 @@
-package gross_calculator;
+package finance;
 
 import java.util.Scanner;
 
 public class GrossPayCalculator {
     public static void main(String[] args) {
         // Get the number of hours worked
-        int hours = 0;
         System.out.println("How many hours did you work?");
-
         Scanner scanner = new Scanner(System.in);
-        hours = scanner.nextInt();
+        int hours = scanner.nextInt();
 
         // Get the hourly pay rate
-        double payRate = 0;
         System.out.println("What is your pay rate?");
-        payRate = scanner.nextDouble();
+        double payRate = scanner.nextDouble();
 
         scanner.close();
 
---
+++ src/finance/QuotaChecker.java
@@ -0,0 +1,26 @@
+package finance;
+
+import java.util.Scanner;
+
+public class QuotaChecker {
+    public static void main(String[] args) {
+
+        //Initialise known values
+        int quota = 10;
+
+        //Get values for the unknown
+        System.out.println("How many sales did you make this week?");
+        Scanner scanner = new Scanner(System.in);
+        int sales = scanner.nextInt();
+        scanner.close();
+
+        //Quick detour for the bonus earners
+        if(sales >= quota){
+            System.out.println("Congrats! You've met your quota.");
+        }
+        else{
+            int salesShort = quota - sales;
+            System.out.println("You did not make your quota. You were " + salesShort + " sales short.");
+        }
+    }
+}
