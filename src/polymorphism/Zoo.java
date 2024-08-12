1de1147970003ef811b5946e8bda77e0c1dcb893
ranga27 <sarang.radke@gmail.com>
2024-08-12 23:27:21 +0100

    Polymorphism - Instanceof

--- src/polymorphism/Zoo.java
+++ src/polymorphism/Zoo.java
@@ -6,11 +6,24 @@ public class Zoo {
         Dog rocky = new Dog();
         rocky.fetch();
         rocky.makeSound();
+        feed(rocky);
 
         Animal masha = new Dog();
         masha.makeSound();
 
         masha = new Cat();
         masha.makeSound();
+        ((Cat)masha).scratch();
+        feed(masha);
+    }
+
+    public static void feed(Animal animal) {
+        if (animal instanceof Dog) {
+            System.out.println("Here's your dog food");
+        }
+        else if (animal instanceof Cat) {
+            System.out.println("Here's your cat food");
+        }
+
     }
 }
